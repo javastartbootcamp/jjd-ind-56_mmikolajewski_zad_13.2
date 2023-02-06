a07c8ccdfe54b3caa4e0fbbedbaed9b62918430a
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-02-06 20:39:30 +0000

    rozwiązanie 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,75 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
+        ArrayList<Integer> numbers = inputNumbers(scanner);
+        printReversedNumbers(numbers);
+        printResultOfAdding(numbers);
+        printTheSmallestNumber(numbers);
+        printTheBiggestNumber(numbers);
+
         // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+    }
+
+    private void printTheSmallestNumber(ArrayList<Integer> numbers) {
+        int smallestNumber = numbers.get(0);
+        for (Integer number : numbers) {
+            if (number < smallestNumber) {
+                smallestNumber = number;
+            }
+        }
+        System.out.println("Najmniejsza liczba w liście to " + smallestNumber);
+    }
+
+    private void printTheBiggestNumber(ArrayList<Integer> numbers) {
+        int biggestNumber = numbers.get(0);
+        for (Integer number : numbers) {
+            if (number > biggestNumber) {
+                biggestNumber = number;
+            }
+        }
+        System.out.println("Największa liczba w liście to " + biggestNumber);
+    }
+
+    private void printResultOfAdding(ArrayList<Integer> numbers) {
+        if (!numbers.isEmpty()) {
+            int sum = 0;
+            StringBuilder sb = new StringBuilder();
+            for (Integer number : numbers) {
+                sb.append(number);
+                sb.append(" + ");
+                sum += number;
+            }
+            sb.replace(sb.length() - 2, sb.length(), "= ");
+            sb.append(sum);
+            System.out.println(sb);
+        }
+
+    }
+
+    private void printReversedNumbers(ArrayList<Integer> numbers) {
+        if (!numbers.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = numbers.size() - 1; i >= 0; i--) {
+                sb.append(numbers.get(i));
+                sb.append(", ");
+            }
+            sb.replace(sb.length() - 2, sb.length(), "");
+            System.out.println(sb);
+        }
+
+    }
+
+    private static ArrayList<Integer> inputNumbers(Scanner scanner) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int status = 1;
+        do {
+            int number = scanner.nextInt();
+            if (number < 0) {
+                status = number;
+                break;
+            }
+            numbers.add(number);
+        } while (status >= 0);
+        return numbers;
     }
 }
